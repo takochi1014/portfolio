30d33245d0079676e45a4a4e0fd33eb8112e030a
takochi1014 <koichitaguchi1014@gmail.com>
2025-04-21 21:14:34 +0900

    update app.js to check workflows

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <h1>こんにちは！私のポートフォリオへようこそ</h1>
       <p>私は◯◯という開発者です。ReactやPythonが好きです。</p>
+      <p>github actoinsによる自動ビルド処理の挙動確認のための文章です。</p>
     </div>
   );
 }
